fe237c13f5089b9a13a05bbe6f5b31d5051ae101
qinyuan <qinyuan15@sina.com>
2015-07-24 00:50:56 +0800

    update

--- src/main/java/com/qinyuan15/lottery/mvc/controller/AdminUserListController.java
+++ src/main/java/com/qinyuan15/lottery/mvc/controller/AdminUserListController.java
@@ -28,13 +28,29 @@ public class AdminUserListController extends ImageController {
     private DatabaseTable getUserTable() {
         String livenessTable = "SELECT spread_user_id,SUM(liveness) AS sum_liveness FROM lottery_liveness " +
                 "WHERE activity_id=(SELECT MAX(activity_id) FROM lottery_activity WHERE expire=false)";
-        String tableName = "user AS u LEFT JOIN (" + livenessTable + ") AS l "
-                + "ON u.id=l.spread_user_id";
+        String tableName = "user AS u LEFT JOIN (" + livenessTable + ") AS l ON u.id=l.spread_user_id";
+
+        String lotTable = "SELECT user_id,MAX(lot_time) AS last_lot_time FROM lottery_lot GROUP BY user_id";
+        tableName += " LEFT JOIN (" + lotTable + ") AS lot ON u.id=lot.user_id";
+
+        tableName += " LEFT JOIN user AS u2 ON u.id=u2.spread_user_id";
+
+        String invitedUserTable = "SELECT u.id,u.spread_user_id,group_concat(u2.username) AS invited_users " +
+                "FROM user AS u JOIN user AS u2 ON u.id=u2.spread_user_id GROUP BY u.id";
+        tableName += " LEFT JOIN (" + invitedUserTable + ") AS idu ON u.id=idu.id";
+
+        tableName += " LEFT JOIN user AS iu ON u.spread_user_id=iu.id" ;
+
+
+
 
         DatabaseTable table = new DatabaseTable(tableName, "u.id", DatabaseTable.QueryType.SQL);
         table.addField("用户名", "u.username", "username");
         table.addField("邮箱", "u.email", "email");
         table.addField("活跃度", "l.sum_liveness", "liveness");
+        table.addField("最近一次抽奖", "lot.last_lot_time", "lot_time");
+        table.addField("邀请了谁","idu.invited_users", "invited_users");
+        table.addField("被请邀请", "iu.username", "invite_user");
         table.addEqualFilter("u.role", User.NORMAL);
         return table;
     }
