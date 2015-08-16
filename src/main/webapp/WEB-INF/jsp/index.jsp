5981eba4f361b29e0f1afc9c8274598f2d3bd2bd
qinyuan <qinyuan15@sina.com>
2015-08-17 02:14:47 +0800

    add icon to control index image switch

--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -6,20 +6,32 @@
     <c:forEach var="indexImageGroup" items="${indexImageGroups}">
         <c:set var="indexImages" value="${indexImageGroup.indexImages}"/>
         <c:set var="firstIndexImage" value="${indexImages[0]}"/>
-        <div class="poster"<c:if test="${firstIndexImage.backPath != null}"> style='background-image: url("${firstIndexImage.backPath}");'</c:if>>
-            <div class="image page-width">
-                <img src="${firstIndexImage.path}" usemap="#indexMap${firstIndexImage.id}"/>
+        <c:set var="multiImage" value="${fn:length(indexImages)>1}"/>
+        <div class="poster" data-options="rowIndex:${indexImageGroup.rowIndex}">
+            <div class="background" <c:if
+                    test="${firstIndexImage.backPath != null}"> style='background-image:url("${firstIndexImage.backPath}");'</c:if>>
+                <div class="image page-width">
+                    <img src="${firstIndexImage.path}" usemap="#indexMap${firstIndexImage.id}"/>
+                </div>
             </div>
+            <c:if test="${multiImage}">
+                <div class="dots">
+                    <c:forEach var="indexImage" items="${indexImages}" varStatus="status">
+                        <div class="dot<c:if test="${status.index == 0}"> selected</c:if>"
+                             data-options="index:${status.index}"></div>
+                    </c:forEach>
+                </div>
+            </c:if>
         </div>
-        <c:if test="${fn:length(indexImages)>1}">
-            <c:forEach var="indexImage" items="${indexImages}" varStatus="status">
-                <script>
-                    <c:if test="${status.index==0}">
-                    indexImageGroups["${indexImageGroup.rowIndex}"] = [];
-                    </c:if>
-                    indexImageGroups["${indexImageGroup.rowIndex}"].push({id:${indexImage.id}, path: "${indexImage.path}", backPath: "${indexImage.backPath}"});
-                </script>
-            </c:forEach>
+        <c:if test="${multiImage}">
+            <script>
+                <c:forEach var="indexImage" items="${indexImages}" varStatus="status">
+                <c:if test="${status.index==0}">
+                indexImageGroups["${indexImageGroup.rowIndex}"] = [];
+                </c:if>
+                indexImageGroups["${indexImageGroup.rowIndex}"].push({index:${status.index}, id:${indexImage.id}, path: "${indexImage.path}", backPath: "${indexImage.backPath}"});
+                </c:forEach>
+            </script>
         </c:if>
     </c:forEach>
 </div>
--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -10,34 +10,69 @@
     }
 
     function changePoster($posterDiv, indexImage) {
-        $posterDiv.hide();
-        $posterDiv.css('background-image', 'url("' + indexImage['backPath'] + '")');
-        $posterDiv.find('img').attr('src', indexImage['path'])
+        $posterDiv.css('min-height', $posterDiv.height() + 'px');
+        var $backgroundDiv = $posterDiv.find('div.background');
+        $backgroundDiv.hide().css('background-image', 'url("' + indexImage['backPath'] + '")');
+        $backgroundDiv.find('img').attr('src', indexImage['path'])
             .attr('usemap', '#indexMap' + indexImage['id']);
-        $posterDiv.fadeIn(1000);
+        $backgroundDiv.fadeIn(1000, function () {
+            $posterDiv.css('min-height', 0);
+        });
+
+        $posterDiv.find('div.dots div.dot').removeClass('selected').eq(indexImage['index']).addClass('selected');
     }
 
-    var indexImageGroups = window['indexImageGroups'];
-    setInterval(function () {
-        for (var rowIndex in indexImageGroups) {
-            if (!(typeof rowIndex == 'string') || !rowIndex.match(/^\d+$/)) {
-                continue;
-            }
+    function switchToNextPoster(rowIndex) {
+        var indexImages = indexImageGroups[rowIndex];
+        var $posterDiv = getPosterDivByRowIndex(rowIndex);
 
-            var indexImages = indexImageGroups[rowIndex];
-            var $div = getPosterDivByRowIndex(rowIndex);
-            var currentImageId = getImageIdByPosterDiv($div);
-
-            for (var i = 0, len = indexImages.length; i < len; i++) {
-                var indexImage = indexImages[i];
-                if (indexImage['id'] == currentImageId) {
-                    if (i == len - 1) {
-                        changePoster($div, indexImages[0]);
-                    } else {
-                        changePoster($div, indexImages[i + 1]);
-                    }
+        var currentImageId = getImageIdByPosterDiv($posterDiv);
+        for (var i = 0, len = indexImages.length; i < len; i++) {
+            var indexImage = indexImages[i];
+            if (indexImage['id'] == currentImageId) {
+                if (i == len - 1) {
+                    changePoster($posterDiv, indexImages[0]);
+                } else {
+                    changePoster($posterDiv, indexImages[i + 1]);
                 }
             }
         }
-    }, window['cycleInterval'] * 1000);
+    }
+
+    var indexImageGroups = window['indexImageGroups'];
+    var cycleInterval = window['cycleInterval'] * 1000;
+    var cycleLock = {};
+    setInterval(function () {
+        for (var rowIndex in indexImageGroups) {
+            if (!indexImageGroups.hasOwnProperty(rowIndex) || !(typeof rowIndex == 'string')
+                || !rowIndex.match(/^\d+$/) || cycleLock[rowIndex] != null) {
+                continue;
+            }
+            switchToNextPoster(rowIndex);
+        }
+    }, cycleInterval);
+
+    $('div.dots div.dot').hover(function () {
+        var $this = $(this);
+        var $posterDiv = $this.getParentByTagNameAndClass('div', 'poster');
+        var rowIndex = $posterDiv.dataOptions('rowIndex');
+        var index = $this.dataOptions('index');
+        cycleLock[rowIndex] = {'ts': new Date().getTime()};
+
+        if (!$this.hasClass('selected')) {
+            var indexImages = indexImageGroups[rowIndex];
+            changePoster($posterDiv, indexImages[parseInt(index)]);
+        }
+    }, function () {
+        var rowIndex = $(this).getParentByTagNameAndClass('div', 'poster').dataOptions('rowIndex');
+        setTimeout(function () {
+            if (cycleLock[rowIndex] == null) {
+                return;
+            }
+            var ts = cycleLock[rowIndex]['ts'];
+            if (new Date().getTime() - ts > cycleInterval) {
+                cycleLock[rowIndex] = null;
+            }
+        }, cycleInterval);
+    });
 })();
