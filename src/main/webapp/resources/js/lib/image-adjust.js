c6c36a8425617d9a558a7c237d664094b97a809c
qinyuan <qinyuan15@sina.com>
2015-11-03 22:18:50 +0800

    patch a bug in image-adjust.js

--- src/main/webapp/resources/js/lib/image-adjust.js
+++ src/main/webapp/resources/js/lib/image-adjust.js
@@ -1,13 +1,38 @@
 function adjustImageWidth(target, width) {
-    adjustImageByRate(target, width / target.width);
+    retryUntilSuccess(function () {
+        var realWidth = target.width;
+        if (realWidth <= 0) {
+            return false;
+        } else {
+            adjustImageByRate(target, width / realWidth);
+            return true;
+        }
+    });
 }
 
 function adjustImageHeight(target, height) {
-    adjustImageByRate(target, height / target.height);
+    retryUntilSuccess(function () {
+        var realHeight = target.height;
+        if (height <= 0) {
+            return false;
+        } else {
+            adjustImageByRate(target, height / realHeight);
+            return true;
+        }
+    });
 }
 
 function adjustImage(target, width, height) {
-    adjustImageByRate(target, Math.min(width / target.width, height / target.height));
+    retryUntilSuccess(function () {
+        var realWidth = target.width;
+        var realHeight = target.height;
+        if (realWidth <= 0 || realHeight <= 0) {
+            return false;
+        } else {
+            adjustImageByRate(target, Math.min(width / realWidth, height / realHeight));
+            return true;
+        }
+    });
 }
 
 function adjustImageByRate(target, rate) {
@@ -17,4 +42,22 @@ function adjustImageByRate(target, rate) {
         target.height = newHeight;
         target.width = newWidth;
     }
+}
+
+function retryUntilSuccess(retryFunction, retryTime, interval) {
+    if (!retryTime) {
+        retryTime = 10;
+    }
+    if (!interval) {
+        interval = 50;
+    }
+    if (!retryFunction()) {
+        var timer = setInterval(function () {
+            if (retryTime <= 1 || retryFunction()) {
+                clearInterval(timer);
+            }
+            retryTime--;
+        }, interval);
+    }
+
 }
