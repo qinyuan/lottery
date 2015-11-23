37ce4cd860ff86381ba299bdfd96c3abf7c0af06
qinyuan <qinyuan15@sina.com>
2015-11-23 22:16:10 +0800

    add DualcoloredBallLotteryLotSerialGeneratorTest

--- src/main/java/com/qinyuan15/lottery/mvc/activity/DualColoredBallLotteryLotSerialGenerator.java
+++ src/main/java/com/qinyuan15/lottery/mvc/activity/DualColoredBallLotteryLotSerialGenerator.java
@@ -10,13 +10,15 @@ import org.apache.commons.lang3.RandomUtils;
 public class DualColoredBallLotteryLotSerialGenerator implements LotteryLotSerialGenerator {
     private final static int MIN_DUAL_COLORED_BALL_NUMBER = 1;
     private final static int MAX_DUAL_COLORED_BALL_NUMBER = 33;
+    private final static int DUAL_COLORED_BALL_NUMBER_LENGTH = String.valueOf(MAX_DUAL_COLORED_BALL_NUMBER).length();
 
     @Override
     public int next() {
         // DecimalFormat  format = new DecimalFormat("00");
         int serialNumber = 0;
+        int multiplier = (int) Math.pow(10, DUAL_COLORED_BALL_NUMBER_LENGTH);
         for (int i = 0; i < 3; i++) {
-            serialNumber = serialNumber * 10 + RandomUtils.nextInt(MIN_DUAL_COLORED_BALL_NUMBER,
+            serialNumber = serialNumber * multiplier + RandomUtils.nextInt(MIN_DUAL_COLORED_BALL_NUMBER,
                     MAX_DUAL_COLORED_BALL_NUMBER + 1);
         }
         return serialNumber;
---
+++ src/test/java/com/qinyuan15/lottery/mvc/activity/DualColoredBallLotteryLotSerialGeneratorTest.java
@@ -0,0 +1,18 @@
+package com.qinyuan15.lottery.mvc.activity;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DualColoredBallLotteryLotSerialGeneratorTest {
+    private DualColoredBallLotteryLotSerialGenerator serialGenerator =
+            new DualColoredBallLotteryLotSerialGenerator();
+
+    @Test
+    public void testNext() throws Exception {
+        for (int i = 0; i < 1000; i++) {
+            assertThat(serialGenerator.next()).isGreaterThanOrEqualTo(10101)
+                    .isLessThanOrEqualTo(333333);
+        }
+    }
+}
